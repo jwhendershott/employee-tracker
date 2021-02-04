3c198153e246502f4d45ebbeb360a5b2555715a3
Jacob Hendershott <jwhendershott@gmail.com>
2021-02-03 21:21:30 -0600

    corrected schema

--- DB/schema.sql
+++ DB/schema.sql
@@ -2,25 +2,25 @@ create database employee_trackerDB;
 
 use employee_trackerDB;
 
-create table employee(
-	id int auto_increment,
-    first_name varchar(30) not null,
-	last_name varchar(30) not null,
-	role_id int not null,
-	manager_id int not null,
-    foreign key (role_id) references role(id),
-    foreign key (manager_id) references employee(id)
+create table department(
+	id int auto_increment primary key,
+    name varchar(30) not null
 );
 
 create table role(
-	id int auto_increment,
+	id int auto_increment primary key,
     title varchar(30) not null,
     salary decimal not null,
     department_id int not null,
     foreign key (department_id) references department(id)
 );
 
-create table department(
-	id int auto_increment,
-    name varchar(30) not null
+create table employee(
+	id int auto_increment primary key,
+    first_name varchar(30) not null,
+	last_name varchar(30) not null,
+	role_id int not null,
+	manager_id int not null,
+    foreign key (role_id) references role(id),
+    foreign key (manager_id) references employee(id)
 );
